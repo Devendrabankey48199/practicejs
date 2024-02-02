01c33c3bf3dc63acf3f6a287cec5c4919264ca95
Devendra Bankey <devendrabankey48199@gmail.com>
2024-02-02 13:49:36 +0530

    Complete logic for Random integer

--- Random-integer-input/index.js
+++ Random-integer-input/index.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const btn = document.querySelector("button");
     btn.addEventListener("click", function() {
+        
+
         let input = document.getElementById("Input")
-        let p = document.querySelector("p");
+        let p1 = document.getElementById("p1");
         let num = Math.floor(Math.random() * 10) + 1; // Random number between 1 and 10
-        p.innerHTML = `Random number is ${num} & Your number is ${input.value}`;
+        p1.innerHTML = `Random number is ${num} & Your number is ${input.value}`;
+       finalResult = (input.value==num)?'Perfect Match':'Try Again'
+        document.getElementById("p2").innerHTML = finalResult
+
 
         
     });
